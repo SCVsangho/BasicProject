86e99f4cb8b45cbaa7315fd75693f5131fb08cee
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-23 19:12:30 +0900

    Version1
    Version1 / 10.23

---
+++ Project/Service/UserLogImpl.java
@@ -0,0 +1,17 @@
+package project.service;
+
+public class UserLogImpl implements Userlog {
+
+	@Override
+	public void newJoin() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void login() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project/Service/Userlog.java
@@ -0,0 +1,12 @@
+package project.service;
+
+public interface Userlog {
+	
+	
+	//회원가입 [[created class 'NewJoinimpl']]
+	public void newJoin();
+	
+	
+	//로그인 [[created class 'LogInimpl']]
+	public void login();
+}
