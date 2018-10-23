d27068112dcef63dfdff73a8fa3ba3e64850204c
jy2058 <44196188+jy2058@users.noreply.github.com>
2018-10-23 21:27:22 +0900

    Update UserCheckImpl.java
    ㅇ

--- Project/dao/UserCheckImpl.java
+++ Project/dao/UserCheckImpl.java
@@ -1,10 +1,26 @@
 package project.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class UserCheckImpl implements UserCheck {
 
 	@Override
 	public void checkedId() {
-		// TODO Auto-generated method stub
+		ArrayList<HashMap<String, String>> join = new ArrayList<HashMap<String,String>>();
+		
+		HashMap<String, String> person = new HashMap<String, String>();
+		for(int i = 0; i < 50; i++){
+			person.put("ID", "id" + i);
+			person.put("PW", "pw" + i);
+			person.put("ADD", "add" + i);
+			person.put("PHONE", "phone" + i);
+			
+		}
+		join.add(person);
+		
+			
+		
 
 	}
 
