797d42637464398d07fcbbd38a2c5662c48da2ac
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-23 19:03:52 +0900

    dao
    dao1

---
+++ Project/UserCheck.java
@@ -0,0 +1,14 @@
+package project.dao;
+
+public interface UserCheck {
+	
+	/*아이디 체크*/
+	public void checkedId();
+	
+	/*회원가입 실행*/
+	public void addId();
+	
+	
+	
+
+}
---
+++ Project/UserCheckImpl.java
@@ -0,0 +1,17 @@
+package project.dao;
+
+public class UserCheckImpl implements UserCheck {
+
+	@Override
+	public void checkedId() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addId() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
