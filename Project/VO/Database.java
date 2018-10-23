ca0d5f47050b9849db53788d3e0ec62f3ac4772c
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-23 19:07:52 +0900

    Version1
    Version1 / 10.23

---
+++ Project/VO/Database.java
@@ -0,0 +1,16 @@
+package project.vo;
+
+import java.util.ArrayList;
+
+
+public class Database {
+
+	/*유저 데이터*/
+	public static ArrayList<UserData> tb_user = new ArrayList<UserData>();
+	
+	/*매뉴 데이터*/
+	public static ArrayList<MenuData> tb_menu = new ArrayList<MenuData>(); 
+	
+	
+	
+}
---
+++ Project/VO/MenuData.java
@@ -0,0 +1,28 @@
+package project.vo;
+
+import java.util.HashSet;
+
+public class MenuData {
+
+	String menuName;
+	int menuPrice;
+
+	
+	/*======================================*/
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public int getMenuPrice() {
+		return menuPrice;
+	}
+
+	public void setMenuPrice(int menuPrice) {
+		this.menuPrice = menuPrice;
+	}
+
+}
