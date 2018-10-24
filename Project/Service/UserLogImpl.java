9dbaf3e5e11aeac11db50ab551cce3757aac8cf1
sunny4299 <44294662+sunny4299@users.noreply.github.com>
2018-10-24 16:58:21 +0900

    1024 version3
    1024 version3

--- Project/Service/UserLogImpl.java
+++ Project/Service/UserLogImpl.java
@@ -1,29 +1,74 @@
-package project.service;
+package Service;
+
+import java.util.Scanner;
 
 public class UserLogImpl implements Userlog {
-
 	@Override
 	public void newJoin() {
-		// TODO Auto-generated method stub
+		System.out.println("[-------회원가입 -------]");
 
 	}
 
 	@Override
 	public void login() {
-		// TODO Auto-generated method stub
+		FindAcount find = new FindAcountImpl();
+		Notice notice= new NoticeImpl();
+		Userlog user = new UserLogImpl();
+		ViewMenu viewmenu = new ViewMenuImpl();
+		
+		Scanner s1 = new Scanner(System.in);
+		
+		System.out.println("[------- 로그인  -------]");
+		
+		//if()
+		
+		boolean want = true;
+		
+		while(want){
+			System.out.println("1. 로그아웃");
+			System.out.println("2. 공지사항");
+			System.out.println("3. 메뉴보기");
+			System.out.println("4. 장바구니");
+			System.out.println("원하시는 메뉴 입력하세요.>");	
+			
+			int click =s1.nextInt();
+
+		
+			
+			switch (click) {
+			case 1:
+				// 로그아웃
+				user.logOut();
+				break;
+			case 2:
+				// 공지사항
+				notice.notice();
+				break;
+			case 3:
+				// 메뉴보기
+				viewmenu.viewMenu();
+				break;
+			case 4:
+				// 장바구니
+				user.logOut();
+				break;
+			}
+		}
+		
 
 	}
 
 	@Override
 	public void logOut() {
-		// TODO Auto-generated method stub
+		System.out.println("[-------로그아웃 되었습니다-------]");
 		
 	}
 
 	@Override
 	public void userList() {
-		// TODO Auto-generated method stub
+		System.out.println("[-------회원목록 -------]");
 		
 	}
+
 
 }
