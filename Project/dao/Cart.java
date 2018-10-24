51eca984a90dadbb6b36ec4e0c0708b29a3d4745
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-24 23:03:00 +0900

    10.25 / Version1
    10.25 / Version1

--- Project/dao/Cart.java
+++ Project/dao/Cart.java
@@ -1,11 +1,9 @@
-package project.dao;
+package project.vo;
 
-public interface Cart {
+import java.util.ArrayList;
 
-	/*고객이 선택하는 ArrayList<MenuData>의 데이터 중 1개를 ArrayList Cart에 데이터 삽입 */
-	public void insertMenuToCart();
+public class Cart {
 	
-	/*고객이 선택하는 ArrayList<MenuData>의 데이터 중 1개를 ArrayList Cart에 데이터에서 삭제*/
-	public void removeMenuToCart();
+	
 	
 }
