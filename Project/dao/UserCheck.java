5b1ebf2f85951ecba5ebb866fb36c741d821c174
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-24 09:09:50 +0900

    10/24 - Version1
    10/24 - Version1

--- Project/dao/UserCheck.java
+++ Project/dao/UserCheck.java
@@ -2,12 +2,14 @@ package project.dao;
 
 public interface UserCheck {
 	
-	/*아이디 체크*/
+	/*아이디 체크 중복확인으로 인해 가입 제한*/
 	public void checkedId();
 	
 	/*회원가입 실행*/
 	public void addId();
 	
+	/*아이디 비밀번호 찾기*/
+	public void findAcount();
 	
 	
 
--- Project/dao/UserCheckImpl.java
+++ Project/dao/UserCheckImpl.java
@@ -7,25 +7,29 @@ public class UserCheckImpl implements UserCheck {
 
 	@Override
 	public void checkedId() {
-		ArrayList<HashMap<String, String>> join = new ArrayList<HashMap<String,String>>();
-		
+
+		ArrayList<HashMap<String, String>> join = new ArrayList<HashMap<String, String>>();
+
 		HashMap<String, String> person = new HashMap<String, String>();
-		for(int i = 0; i < 50; i++){
+		for (int i = 0; i < 50; i++) {
 			person.put("ID", "id" + i);
 			person.put("PW", "pw" + i);
 			person.put("ADD", "add" + i);
 			person.put("PHONE", "phone" + i);
-			
+
 		}
 		join.add(person);
-		
-			
-		
 
 	}
 
 	@Override
 	public void addId() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findAcount() {
 		// TODO Auto-generated method stub
 
 	}
