4153fd9ebcb6cd96ff58ed37880c7b5cff25bd4c
jy2058 <44196188+jy2058@users.noreply.github.com>
2018-10-24 21:00:53 +0900

    Update FindAcountImpl.java
    기능구현 완료

--- Project/Service/FindAcountImpl.java
+++ Project/Service/FindAcountImpl.java
@@ -1,11 +1,39 @@
 package project.service;
 
+import java.util.Scanner;
+
+import project.vo.Database;
+import project.vo.UserData;
+
 public class FindAcountImpl implements FindAcount {
 
 	@Override
-	public void FindAcount() {
-		// TODO Auto-generated method stub
+	public void findAcount() {
 
+		String name;
+		String id;
+		String tel;
+		boolean flag = false;
+
+		Scanner s = new Scanner(System.in);
+
+		while (!flag) {
+			System.out.println("이름 입력 : ");
+			name = s.nextLine();
+			System.out.println("아이디 입력 : ");
+			id = s.nextLine();
+			System.out.println("전화번호 입력 : ");
+			tel = s.nextLine();
+
+			for (int i = 0; i < Database.tb_user.size(); i++) {
+				UserData user = Database.tb_user.get(i);
+				if (user.getName().equals(name) && user.getId().equals(id) && user.getTel().equals(tel)) {
+					System.out.println("패스워드: " + user.getPassWord());
+					flag = true;
+				} else {
+					System.out.println("등록되지 않은 정보입니다.");
+				}
+			}
+		}
 	}
-
 }
