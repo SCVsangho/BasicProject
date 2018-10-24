f3d715a5b25df7778050bcb513cde74c8e4f5405
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-24 23:00:31 +0900

    10.24 / Version1
    10.24 / Version1

--- Project/Controller/Controller.java
+++ Project/Controller/Controller.java
@@ -1,19 +1,15 @@
-package Controller;
+package project.controller;
 
 import java.util.Scanner;
 
-import Service.FindAcount;
-import Service.FindAcountImpl;
-import Service.Notice;
-import Service.NoticeImpl;
-import Service.UserLogImpl;
-import Service.Userlog;
-import Service.ViewMenu;
-import Service.ViewMenuImpl;
-
-
-
-
+import project.service.FindAcount;
+import project.service.FindAcountImpl;
+import project.service.Notice;
+import project.service.NoticeImpl;
+import project.service.UserLogImpl;
+import project.service.Userlog;
+import project.service.ViewMenu;
+import project.service.ViewMenuImpl;
 
 public class Controller {
 
@@ -24,48 +20,47 @@ public class Controller {
 
 		while (isContinue) {
 
-			System.out.println("【 메뉴를 선택해주세요 】");
-			System.out.println("1. 회원가입");
-			//System.out.println("2.회원목록");
-			System.out.println("2. 로그인");
-			System.out.println("3. 아이디,비밀번호 찾기");
-			System.out.println("4. 메뉴보기");
-			System.out.println("5. 공지사항");
+			System.out.println("메뉴를 선택해주세요.");
+			System.out.println("1.회원가입");
+			System.out.println("2.회원목록");
+			System.out.println("3.로그인");
+			System.out.println("4.아이디, 비밀번호 찾기");
+			System.out.println("5.메뉴보기");
+			System.out.println("6.공지사항");
 			System.out.println("메뉴에 해당하는 번호 입력 >");
 
 			int menu = s.nextInt();
 
 			FindAcount find = new FindAcountImpl();
-			Notice notice= new NoticeImpl();
+			Notice notice = new NoticeImpl();
 			Userlog user = new UserLogImpl();
 			ViewMenu viewmenu = new ViewMenuImpl();
-			
-			/*case 2:
-			// 회원목록
-			user.userList();
-			break;*/
-			
-			
+
 			switch (menu) {
 			case 1:
 				// 회원가입
 				user.newJoin();
 				break;
 			case 2:
+				// 회원목록
+				user.userList();
+				break;
+			case 3:
 				// 로그인
 				user.login();
-				break;		
-			case 3:
-				//아이디, 비밀번호 찾기
-				find.findAcount();
 				break;
+
 			case 4:
-				//메뉴보기
-				viewmenu.addMenu();
+				// 아이디, 비밀번호 찾기
+				find.findAcount(null);
 				break;
 			case 5:
+				// 메뉴보기
+				viewmenu.addMenu();
+				break;
+			case 6:
 				// 공지사항
-				notice.notice();
+				notice.viewNotice();
 				break;
 			default:
 				System.out.println("프로그램이 종료되었습니다.");
@@ -75,6 +70,6 @@ public class Controller {
 			}
 
 		}
-		
 	}
+
 }
