258ba125bfff5a7d77bced12d7f2b7b87d1918a4
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-24 16:48:34 +0900

    2018 10. 24  version 2
    2018 10. 24  version 2

---
+++ Project/Service/cartin.java
@@ -0,0 +1,14 @@
+package project.service;
+
+public interface cartin {
+	
+	
+	/*장바구니에 매뉴를 추가*/
+	void insertCart();
+	
+	
+	/*장바구니에서 매뉴를 삭제*/
+	void removeCart();
+	
+
+}
---
+++ Project/Service/cartinImpl.java
@@ -0,0 +1,17 @@
+package project.service;
+
+public class cartinImpl implements cartin {
+
+	@Override
+	public void insertCart() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeCart() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
