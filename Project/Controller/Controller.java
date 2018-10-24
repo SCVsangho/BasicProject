c3af9344412c117fae2b3231d36c696b2c96e266
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-24 16:49:22 +0900

    2018 10. 24  version 2
    2018 10. 24  version 2

--- Project/Controller/Controller.java
+++ Project/Controller/Controller.java
@@ -1,9 +1,75 @@
 package project.controller;
 
+import java.util.Scanner;
+
+import project.service.FindAcount;
+import project.service.FindAcountImpl;
+import project.service.Notice;
+import project.service.NoticeImpl;
+import project.service.UserLogImpl;
+import project.service.Userlog;
+import project.service.ViewMenu;
+import project.service.ViewMenuImpl;
+
 public class Controller {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 
+		boolean isContinue = true;
+
+		while (isContinue) {
+
+			System.out.println("메뉴를 선택해주세요.");
+			System.out.println("1.회원가입");
+			System.out.println("2.회원목록");
+			System.out.println("3.로그인");
+			System.out.println("4.아이디, 비밀번호 찾기");
+			System.out.println("5.메뉴보기");
+			System.out.println("6.공지사항");
+			System.out.println("메뉴에 해당하는 번호 입력 >");
+
+			int menu = s.nextInt();
+
+			FindAcount find = new FindAcountImpl();
+			Notice notice = new NoticeImpl();
+			Userlog user = new UserLogImpl();
+			ViewMenu viewmenu = new ViewMenuImpl();
+
+			switch (menu) {
+			case 1:
+				// 회원가입
+				user.newJoin();
+				break;
+			case 2:
+				// 회원목록
+				user.userList();
+				break;
+			case 3:
+				// 로그인
+				user.login();
+				break;
+
+			case 4:
+				// 아이디, 비밀번호 찾기
+				find.findAcount(null);
+				break;
+			case 5:
+				// 메뉴보기
+				viewmenu.addMenu();
+				break;
+			case 6:
+				// 공지사항
+				notice.notice();
+				break;
+			default:
+				System.out.println("프로그램이 종료되었습니다.");
+				isContinue = false;
+				break;
+
+			}
+
+		}
 	}
 
 }
