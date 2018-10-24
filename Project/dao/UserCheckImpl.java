6a5bba75ffeb27039d68c2644599d65682ac6235
jy2058 <44196188+jy2058@users.noreply.github.com>
2018-10-24 15:32:01 +0900

    Update UserCheckImpl.java
    수정중

--- Project/dao/UserCheckImpl.java
+++ Project/dao/UserCheckImpl.java
@@ -1,34 +1,12 @@
 package project.dao;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import project.vo.Database;
 import project.vo.UserData;
 
 public class UserCheckImpl implements UserCheck {
 
-	@Override
-	public void checkedId() {
-		ArrayList<HashMap<String, String>> join = new ArrayList<HashMap<String,String>>();
-		
-		HashMap<String, String> person = new HashMap<String, String>();
-		for(int i = 0; i < 50; i++){
-			person.put("ID", "id" + i);
-			person.put("PW", "pw" + i);
-			person.put("ADD", "add" + i);
-			person.put("PHONE", "phone" + i);
-			
-		}
-		join.add(person);
-		
-			
-		
-
-	}
-	
-	//아이디 중복 확인
-	public UserData selectPerson(String key, String value) {	
+	public UserData checkedId(String key, String value) {
 		for(int i = 0; i < Database.tb_user.size(); i++){
 			UserData person = Database.tb_user.get(i);
 			
@@ -38,14 +16,20 @@ public class UserCheckImpl implements UserCheck {
 				}
 
 			}
+			
 		}
 		return null;
+
 	}
 
 	@Override
 	public void addId() {
 		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public void findAcount(){
+		
 	}
 
 }
