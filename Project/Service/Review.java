db6d23654167ca1b149ca8cc0f5dfeb792151e60
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-25 20:25:58 +0900

    10.25 / Version 1
    리뷰 서비스 생성

---
+++ Project/Service/Review.java
@@ -0,0 +1,14 @@
+package project.service;
+
+public interface Review {
+	
+	
+	/*리뷰 작성*/
+	void writeReview();
+	
+	/*리뷰 삭제*/
+	void removeReview();
+	
+	/*리뷰 보기*/
+	void viewReview();
+}
---
+++ Project/Service/ReviewImpl.java
@@ -0,0 +1,23 @@
+package project.service;
+
+public class ReviewImpl implements Review{
+
+	@Override
+	public void writeReview() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeReview() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewReview() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
