09bb650b5762242f7d79aee55227dc6c60882fac
sunny4299 <44294662+sunny4299@users.noreply.github.com>
2018-10-25 14:00:32 +0900

    10.25- Version2
    10.25- Version2

--- Project/Service/ViewMenu.java
+++ Project/Service/ViewMenu.java
@@ -2,11 +2,12 @@ package project.service;
 
 public interface ViewMenu {
 	
-	//매뉴 보기 [[created class 'ViewMenuImpl']]
+	//매뉴 추가 [[created class 'ViewMenuImpl']]
 	public void addMenu();
 	
+	//메뉴보기
 	public void viewMenu();
 	
-	
-
+	//메뉴삭제
+	public void removeMenu(); 
 }
