2045039f9cfd0c1f7b2684d957f9c8c2b93f8b1e
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-25 20:27:57 +0900

    10.25 Version1
    리뷰 데이터베이스 생성

---
+++ Project/vo/ReviewData.java
@@ -0,0 +1,60 @@
+package project.vo;
+
+import java.util.Date;
+
+public class ReviewData {
+	
+	int number;
+	String description;
+	String author;		 // 현재 로그인 된 아이디를 강제로 저장
+	String time;		// 작성 시 yyyy.MM.dd : hh.mm
+	Date timeS;
+
+	/*
+	 * ====================================================================================
+	 */
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTime(String time) {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public Date getTimeS() {
+		return timeS;
+	}
+
+	public void setTimeS(Date today) {
+		this.timeS = today;
+	}
+
+}
+
+
