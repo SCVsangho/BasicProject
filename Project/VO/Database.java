0804ad0fd2e09f684891f553a41d9123321114ee
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-25 10:17:31 +0900

    10.25 / Version2
    공지사항 데이터베이스 수정

--- Project/VO/Database.java
+++ Project/VO/Database.java
@@ -12,7 +12,7 @@ public class Database {
 	public static ArrayList<MenuData> tb_menu = new ArrayList<MenuData>(); 
 	
 	/*공지사항 데이터*/
-	public static ArrayList<Notice> tb_notice = new ArrayList<Notice>(); 
+	public static ArrayList<NoticeData> tb_notice = new ArrayList<NoticeData>(); 
 	
 	/*장바구니*/
 	public static ArrayList<MenuData> tb_cart = new ArrayList<MenuData>();
