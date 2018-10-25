93f688dc6e3ab299f91cb7f756a10c4c14400b0d
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-25 17:07:52 +0900

    10.25  Version1
    10.25  Version1

---
+++ Project/dao/PayDao.java
@@ -0,0 +1,8 @@
+package project.dao;
+
+public interface PayDao {
+	
+	/*영수증 출력*/
+	void receipt();
+
+}
---
+++ Project/dao/PayDaoImpl.java
@@ -0,0 +1,13 @@
+package project.dao;
+
+import project.service.Pay;
+
+public class PayDaoImpl implements PayDao {
+	
+	@Override
+	public void receipt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
