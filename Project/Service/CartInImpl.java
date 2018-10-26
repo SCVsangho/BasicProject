26af9502930fdb5e3351fe69e1bc67b12c8177a6
JangGooHyeon <44196180+JangGooHyeon@users.noreply.github.com>
2018-10-26 15:55:58 +0900

    Update CartInImpl.java

--- Project/Service/CartInImpl.java
+++ Project/Service/CartInImpl.java
@@ -9,50 +9,26 @@ public class CartInImpl implements CartIn {
 
 	@Override
 	public void insertCart() {
+		CartDao c1 = new CartIDaoImpl();
 		ViewMenu viewM = new ViewMenuImpl();
-		// 메뉴리스트 호출
 
-		Scanner s = new Scanner(System.in);
-
+//		viewM.viewMenu(); 
 		boolean flag = false;
+
 		while (!flag) {
-			System.out.println("메뉴를 선택해주세요.");
-			viewM.viewMenu();
-			String addMenu = s.nextLine();
-
-			for (int i = 0; i < Database.tb_menu.size(); i++) {
-				MenuData menu = Database.tb_cart.get(i);
-				if (menu.equals(addMenu)) {
-
-//					insertMenuToCart(); // 맞는지 모르겠어요. 메서드만 적혀있더라구요.
-//					.set 필요?
-					System.out.println("장바구니에 메뉴가 저장되었습니다.");
-
-					System.out.println("메뉴선택을 계속 하시겠습니까?");
-					System.out.println("계속하려면 [y], 그만하려면 [n]을 누르세요 ");
-					String sc = s.nextLine();
-
-					if (sc.equals("Y") || sc.equals("y")) {
-						System.out.println("메뉴 선택을 계속합니다.");
-						insertCart();
-
-					} else {
-//						insertMenuToCart(); //필요한지 모르겠어요. 저장이 한번 더 되는거같은데 좀 헷갈려요. 
-//						.set 필요? 
-						System.out.println("장바구니를 저장하고 종료합니다.");
-						// insertMenuToCart();가 필요없으면 "장바구니를 종료합니다."
-						flag = true;
-					}
-
-				} else {
-					System.out.println("이 메뉴는 없는 메뉴입니다. 다시 입력해주세요.");
-					insertCart();
-				}
-
+			Scanner s = new Scanner(System.in);
+			System.out.println("[[ 선택 :번호입력  || 종료  :0번 입력 ]]");
+			System.out.print("메뉴를 선택해주세요.");
+			int selectMenu = s.nextInt(); 
+			if (selectMenu == 0) {
+				flag = true;
+			} else {
+				int indexValue = selectMenu - 1;
+													
+													
+				// pay.indexValue.add(indexValue); 
+				c1.insertMenuToCart(indexValue);
 			}
-			// 실행 돌리면 메뉴선택에서 입력값 받아도 안넘어갑니다.
-			// if문에 문제가 있는거 같은데 파악이 안되서 수정 못하고 있습니다.
-			// 학원가서 수정할게요.
 		}
 
 	}
