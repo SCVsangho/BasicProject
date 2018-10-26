1df44770768f885c924d902eb67e3cc731be3845
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-26 19:04:55 +0900

    10.26  / 19:04
    10.26  / 19:04

--- Project/dao/CartDao.java
+++ Project/dao/CartDao.java
@@ -1,9 +1,9 @@
 package project.dao;
 
-public interface Cart {
+public interface CartDao {
 
 	/*고객이 선택하는 ArrayList<MenuData>의 데이터 중 1개를 ArrayList Cart에 데이터 삽입 */
-	public void insertMenuToCart();
+	public void insertMenuToCart(int index);
 	
 	/*고객이 선택하는 ArrayList<MenuData>의 데이터 중 1개를 ArrayList Cart에 데이터에서 삭제*/
 	public void removeMenuToCart();
