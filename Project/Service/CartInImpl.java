edef6a6b7130310e4e2390d9f072dbe97792d8f3
SCVsangho <44111778+SCVsangho@users.noreply.github.com>
2018-10-28 14:35:23 +0900

    new final 10.28 // 14:35
    new final

--- Project/Service/CartInImpl.java
+++ Project/Service/CartInImpl.java
@@ -13,6 +13,8 @@ public class CartInImpl implements CartIn {
 		CartDao c1 = new CartDaoImpl();
 		ViewMenu viewM = new ViewMenuImpl();
 
+		System.out.println("******[[ 장바구니 담기 ]]******");
+
 		viewM.viewMenu();
 		boolean flag = false;
 
@@ -27,7 +29,7 @@ public class CartInImpl implements CartIn {
 				int indexValue = selectMenu - 1;
 				System.out.println(Database.tb_menu.get(indexValue).getMenuName() + " (이)가 장바구니에 추가되었습니다.");
 				PayImpl.indexValue.add(indexValue);
-				c1.insertMenuToCart((int)indexValue);
+				c1.insertMenuToCart((int) indexValue);
 			}
 		}
 
@@ -35,8 +37,26 @@ public class CartInImpl implements CartIn {
 
 	@Override
 	public void removeCart() {
-		// TODO Auto-generated method stub
+		CartDao c1 = new CartDaoImpl();
+		ViewMenu viewM = new ViewMenuImpl();
 
+		System.out.println("******[[ 장바구니 삭제 ]]******");
+
+		viewM.viewMenu();
+		boolean flag = false;
+
+		while (!flag) {
+			Scanner s = new Scanner(System.in);
+			System.out.println("[[ 선택 : 번호입력  ||  종료 : 0번 입력 ]]");
+			System.out.print("ANSWER >>");
+			int selectMenu = s.nextInt();
+			if (selectMenu == 0) {
+				flag = true;
+			} else {
+				int indexValue = selectMenu - 1;
+				System.out.println(Database.tb_cart.remove(indexValue) + " (이)가 장바구니에서 삭제되었습니다.");
+			}
+		}
 	}
 
 	@Override
@@ -45,15 +65,14 @@ public class CartInImpl implements CartIn {
 		System.out.println("******[[ " + Database.loginUser.getId() + "님의 장바구니 ]]******");
 		for (int i = 0; i < Database.tb_cart.size(); i++) {
 			System.out.println("--((" + (i + 1) + "))-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-			System.out.println( "[메뉴이름]  : " + Database.tb_cart.get(i).getMenuName());
-			System.out.println( "[메뉴가격]  : " + Database.tb_cart.get(i).getMenuPrice());
-			 resultP += Database.tb_cart.get(i).getMenuPrice();
-			
+			System.out.println("[메뉴이름]  : " + Database.tb_cart.get(i).getMenuName());
+			System.out.println("[메뉴가격]  : " + Database.tb_cart.get(i).getMenuPrice());
+			resultP += Database.tb_cart.get(i).getMenuPrice();
+
 		}
 		System.out.println("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
-		System.out.println("총 "+ Database.tb_cart.size() + "개");
+		System.out.println("총 " + Database.tb_cart.size() + "개");
 		System.out.println("전체 예정결제금액  : " + resultP + "원");
-		
 
 	}
 
